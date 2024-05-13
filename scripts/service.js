04cffbc16e4ac04205b941aac9e125bd129cd7cb
ashishshres <ashishshr27@gmail.com>
2024-05-13 18:38:43 +0545

    add service.js

---
+++ scripts/service.js
@@ -0,0 +1,33 @@
+let serviceBtn1 = document.querySelector(".service1");
+let serviceBtn2 = document.querySelector(".service2");
+let serviceBtn3 = document.querySelector(".service3");
+let serviceBtn4 = document.querySelector(".service4");
+let serviceBtn5 = document.querySelector(".service5");
+let serviceBtn6 = document.querySelector(".service6");
+let serviceBtn7 = document.querySelector(".service7");
+let serviceBtn8 = document.querySelector(".service8");
+
+serviceBtn1.addEventListener("click", () => {
+    window.location.href = "#service1";
+});
+serviceBtn2.addEventListener("click", () => {
+    window.location.href = "#service2";
+});
+serviceBtn3.addEventListener("click", () => {
+    window.location.href = "#service3";
+});
+serviceBtn4.addEventListener("click", () => {
+    window.location.href = "#service4";
+});
+serviceBtn5.addEventListener("click", () => {
+    window.location.href = "#service5";
+});
+serviceBtn6.addEventListener("click", () => {
+    window.location.href = "#service6";
+});
+serviceBtn7.addEventListener("click", () => {
+    window.location.href = "#service7";
+});
+serviceBtn8.addEventListener("click", () => {
+    window.location.href = "#service8";
+});

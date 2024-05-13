3b58632f3f9752e6d0bd570683bbf55c8859e3c4
ashishshres <ashishshr27@gmail.com>
2024-05-13 18:43:15 +0545

    fix: change variable

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 let homeServiceBtn = document.querySelectorAll(".home-service-btn");
-allServiceBtn.forEach((btn) => {
+homeServiceBtn.forEach((btn) => {
     btn.addEventListener("click", function () {
         window.location.href = "./pages/services.html";
     });
